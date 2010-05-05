472c6661884b6c3344804698266a20a15c918a93
dmail07 <dmail07@8169190b-fedb-79aa-6a5a-a987b9c8a9c4>
2010-05-05 01:38:01 +0000

    Fiz um exemplo de cria e lista com o Reclamacao.
    Adicionei um template index.html de página inicial.
    
    Adicionei CSS e ativei o static serve (para servir imagens, CSS, js...)

--- trunk/Pizzer/urls.py
+++ trunk/Pizzer/urls.py
@@ -1,4 +1,6 @@
 from django.conf.urls.defaults import *
+from django.views.generic.simple import direct_to_template
+from django.views import static
 
 # Uncomment the next two lines to enable the admin:
 # from django.contrib import admin
@@ -6,7 +8,10 @@ from django.conf.urls.defaults import *
 
 urlpatterns = patterns('',
     # Example:
+    (r'^pizzer/$', direct_to_template, {'template': 'index.html'}),
     (r'^pizzer/', include('Pizzer.modulo_reclamacoes.urls')),
+
+    (r'^pizzer/media/(?P<path>.*)$', static.serve, {'document_root': 'media'}),
 
     # Uncomment the admin/doc line below and add 'django.contrib.admindocs'
     # to INSTALLED_APPS to enable admin documentation:
