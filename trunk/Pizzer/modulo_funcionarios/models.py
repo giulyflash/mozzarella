a5a573ec7468b626f3f9cf3e13c2979662e1fccc
daniel.interliche <daniel.interliche@8169190b-fedb-79aa-6a5a-a987b9c8a9c4>
2010-05-07 17:56:09 +0000

    Cadastro de funcionarios sem validacao e persistencia no banco de dados

--- trunk/Pizzer/modulo_funcionarios/models.py
+++ trunk/Pizzer/modulo_funcionarios/models.py
@@ -1,10 +1,37 @@
 # -*- coding: utf-8 -*-
 from django.db import models
+from django.forms import ModelForm
 
-# Create your models here.
+FUNCAO_CHOICES = (
+    ('Pizzaiolo', 'Pizzaiolo'),
+    ('Atendente', 'Atendente'),
+    ('Gerente', 'Gerente'),
+)
 
-class Funcionario(models.Model):  # Futuramente a classe funcionario herdara da classe pessoa
-    cpf = models.CharField(max_length=20)
-	
-    def get_absolute_url(self):
-        return '/pizzer/'
+PERIODO_CHOICES = (
+    ('T', 'Tarde'),
+    ('N', 'Noite'),
+    ('N+T', 'Tarde+Noite'),
+)
+
+class Pessoa(models.Model):
+    nome = models.CharField(max_length = 50)
+    endereco = models.CharField(max_length = 100)
+    telefone = models.CharField(max_length = 15)
+
+class Funcionario(Pessoa):
+    cpf = models.CharField(max_length = 20, primary_key = True)
+    rg = models.CharField(max_length = 15)
+    salario = models.DecimalField(max_digits = 10, decimal_places = 2)
+    funcao = models.CharField(max_length = 10, choices = FUNCAO_CHOICES)
+    periodo = models.CharField(max_length = 11, choices = PERIODO_CHOICES)
+
+    def __str__(self): #quando o metodo str() da instancia de Funcionario e' chamado o objeto retorna o nome do funcionario
+        return self.nome
+
+    def get_absolute_url(self): #retorna o URL absoluto da instancia de Funcionario
+        return "/pizzer/"
+
+class FuncionarioForm(ModelForm):
+    class meta:
+        model = Funcionario
