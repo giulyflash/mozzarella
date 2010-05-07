0aaa6904cf4a11a312d814b7aec8302056b29ec1
daniel.interliche <daniel.interliche@8169190b-fedb-79aa-6a5a-a987b9c8a9c4>
2010-05-07 19:04:27 +0000

    Criei o módulo de pedidos incluindo models Pedido e StatusItemPedido além dos urls desse módulo

---
+++ trunk/Pizzer/modulo_pedidos/models.py
@@ -0,0 +1,29 @@
+# -*- coding: utf-8 -*-
+from django.db import models
+from django.forms import ModelForm
+
+STATUS_PEDIDO_CHOICES = (  # e.g. todos itens atendidos
+    ('NA', 'Todos itens prontos'),
+    ('SE', 'Sendo entregue pelo motoqueiro'),
+    ('E', 'Entrada'),
+    ('NPE', 'Nao Pode ser entregue'),
+)
+
+STATUS_ITEM_PEDIDO_CHOICES = (  # e.g. pizza pronta, bebida entregue
+    ('NA', 'Nao atendido'),
+    ('EP', 'Em preparo'),
+    ('P', 'Pronto'),
+)
+
+class StatusItemPedido(models.Model):
+    pedido = models.ForeignKey(Pedido)
+    item_cardapio = models.ForeignKey(ItemCardapio)
+    status = models.CharField(choices=STATUS_ITEM_PEDIDO_CHOICES)
+
+class Pedido(models.Model):
+    dono = models.ForeignKey(Cliente)
+    itens_cardapio = ManyToManyKey(ItemCardapio, through=StatusItemPedido)
+    status = models.CharField(choices=STATUS_CHOICES)
+    data_horario = models.DateTimeField(auto_now=True)
+
+
