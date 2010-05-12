90dec12c012d1e74992ba790f2bd953ed7d2b188
marceloangelopita <marceloangelopita@8169190b-fedb-79aa-6a5a-a987b9c8a9c4>
2010-05-12 14:07:35 +0000

    criação do modulo bebidas.

--- trunk/Pizzer/modulo_bebidas/views.py
+++ trunk/Pizzer/modulo_bebidas/views.py
@@ -1,1 +1,11 @@
-# Create your views here.
+# -*- coding: utf-8 -*-
+
+from models import Bebida
+from django.db.models import Q
+
+from utils.views import lista_objetos
+
+def lista_bebidas(request):
+    nome = request.GET.get('nome')  # Obten��o dos par�metros do request
+    consulta = Q(nome__icontains=nome)
+    return lista_objetos(request, [nome], Bebida, 'listagem_bebidas.html', 'bebidas', consulta)# Create your views here.
