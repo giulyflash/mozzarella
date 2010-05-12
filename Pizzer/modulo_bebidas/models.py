e81244577e94c055fd4333a65dd8de4ef76805bc
marceloangelopita <marceloangelopita@8169190b-fedb-79aa-6a5a-a987b9c8a9c4>
2010-05-12 14:07:35 +0000

    criação do modulo bebidas.

--- Pizzer/modulo_bebidas/models.py
+++ Pizzer/modulo_bebidas/models.py
@@ -1,3 +1,9 @@
+# -*- coding: utf-8 -*-
 from django.db import models
+from modulo_pizzas.models import ItemCardapio
+
+class Bebida(ItemCardapio):
+    quantidade = models.IntegerField()
 
 # Create your models here.
+
