c8e8eaad4439cfd6b0f25b569b1a8b62ba4a7406
daniel.interliche <daniel.interliche@8169190b-fedb-79aa-6a5a-a987b9c8a9c4>
2010-05-12 13:44:06 +0000

    criação do model Ingrediente

--- trunk/Pizzer/modulo_ingredientes/models.py
+++ trunk/Pizzer/modulo_ingredientes/models.py
@@ -1,3 +1,19 @@
+# -*- coding: utf-8 -*-
 from django.db import models
+from django.forms import ModelForm
 
-# Create your models here.
+class Ingrediente(models.Model):
+    nome = models.CharField(max_length=25, primary_key=True)
+    preco = models.DecimalField(max_digits=3, decimal_places=2)
+    custo = models.DecimalField(max_digits=3, decimal_places=2)
+    quantidade = models.IntegerField()
+
+    def __str__(self):
+        return self.nome
+
+    def get_absolute_url(self):
+        return "/pizzer/"
+
+class IngredientesForm(ModelForm):
+    class meta:
+        model = Ingrediente
