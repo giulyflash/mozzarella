a33a30799f3f3d36caa3f7794565447e03b77a90
eduardo.marubayashi@gmail.com <eduardo.marubayashi@gmail.com@8169190b-fedb-79aa-6a5a-a987b9c8a9c4>
2010-05-12 14:29:01 +0000

    Pedi

--- trunk/Pizzer/modulo_pedidos/urls.py
+++ trunk/Pizzer/modulo_pedidos/urls.py
@@ -1,13 +1,24 @@
 from django.conf.urls.defaults import *
-from django.views.generic.create_update import create_object
-from django.views.generic.list_detail import object_list
-from modulo_reclamacoes.models import Pedido
+from django.views.generic.create_update import create_object, update_object, delete_object
+from modulo_pedidos.models import Pedido
+from views import *
 
-model_info = {
-    "model": Pedido,
-    "template_name": "pda_criacao_pedido.html"
-}
+# Uncomment the next two lines to enable the admin:
+# from django.contrib import admin
+# admin.autodiscover()
 
 urlpatterns = patterns('',
-    (r'^pda/pedido/cria/', create_object, model_info),
+    # Example:
+    (r'^pedido/cria/$', create_object, {'model': Pedido, 'template_name': 'criacao_pedido.html'}),
+    #(r'^pedido/edita/(?P<object_id>\d+)/$', update_object, {'model': Pedido, 'template_name': 'edita_pedido.html'}),
+    #(r'^pedido/deleta/(?P<object_id>\d+)/$', delete_object, {'model': Pedido, 'template_name': 'confirmacao_delecao.html',
+    #                                                             'post_delete_redirect': '/pizzer/pedidos/', 'extra_context': {'model': Pedido}}),
+    #(r'^pedidos/$', lista_pedidos)
+
+    # Uncomment the admin/doc line below and add 'django.contrib.admindocs'
+    # to INSTALLED_APPS to enable admin documentation:
+    # (r'^admin/doc/', include('django.contrib.admindocs.urls')),
+
+    # Uncomment the next line to enable the admin:
+    # (r'^admin/', include(admin.site.urls)),
 )
