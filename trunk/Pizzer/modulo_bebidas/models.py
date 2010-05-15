7987a9f2aa2ff1ad45bd4e7ba3d3294969e0e4d9
marceloangelopita <marceloangelopita@8169190b-fedb-79aa-6a5a-a987b9c8a9c4>
2010-05-15 13:51:03 +0000

    errinho corrigido

--- trunk/Pizzer/modulo_bebidas/models.py
+++ trunk/Pizzer/modulo_bebidas/models.py
@@ -4,6 +4,8 @@ from modulo_pizzas.models import ItemCardapio
 
 class Bebida(ItemCardapio):
     quantidade = models.IntegerField()
+    def get_absolute_url(self):
+        return '/pizzer/'
 
 # Create your models here.
 
