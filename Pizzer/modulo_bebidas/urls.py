94f49a9eb5a77bb801f0c60462cda95080ae03c0
marceloangelopita <marceloangelopita@8169190b-fedb-79aa-6a5a-a987b9c8a9c4>
2010-05-15 13:51:03 +0000

    errinho corrigido

--- Pizzer/modulo_bebidas/urls.py
+++ Pizzer/modulo_bebidas/urls.py
@@ -8,6 +8,6 @@ urlpatterns = patterns('',
     (r'^bebida/cria/$', create_object, {'model': Bebida, 'template_name': 'criacao_bebida.html'}),
     (r'^bebida/edita/(?P<object_id>\d+)/$', update_object, {'model': Bebida, 'template_name': 'edicao_bebida.html'}),
     (r'^bebida/deleta/(?P<object_id>\d+)/$', delete_object, {'model': Bebida, 'template_name': 'confirmacao_delecao.html',
-                                                                 'post_delete_redirect': '/pizzer/clientes/', 'extra_context': {'model': Bebida}}),
+                                                                 'post_delete_redirect': '/pizzer/bebidas/', 'extra_context': {'model': Bebida}}),
     (r'^bebidas/$', lista_bebidas)
 )
