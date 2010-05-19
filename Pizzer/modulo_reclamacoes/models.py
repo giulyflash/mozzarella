04403d981b58f3bf1c7f690967bb848b049e37c5
dmail07 <dmail07@8169190b-fedb-79aa-6a5a-a987b9c8a9c4>
2010-05-19 13:07:22 +0000

    campo cliente em Reclamacao agora é obrigatório

--- Pizzer/modulo_reclamacoes/models.py
+++ Pizzer/modulo_reclamacoes/models.py
@@ -5,13 +5,10 @@ from django.forms import ModelForm
 from django.forms.widgets import TextInput, Textarea, Select
 from modulo_clientes.models import Cliente
 
-# Create your models here.
-
 class Reclamacao(models.Model):  # Note que Pessoa herda da classe Model, que está dentro do módulo models
-    cliente = models.ForeignKey(Cliente, null=True, blank=True)  # null=True temporário até implementarem Cliente
+    cliente = models.ForeignKey(Cliente, null=True)  # null=True temporário até implementarem Cliente
     assunto = models.CharField(max_length=60)
     texto = models.TextField()
-    resposta = models.TextField(null=True)
 
     def __str__(self):
         return self.assunto
@@ -22,7 +19,6 @@ class Reclamacao(models.Model):  # Note que Pessoa herda da classe Model, que es
 class ReclamacaoFormCliente(ModelForm):
     class Meta:
         model = Reclamacao
-        exclude = ['resposta']
 
 class ReclamacaoFormGerente(ModelForm):
     cliente = forms.ComboField(required=False, widget=Select(attrs={'readonly': True}))  # O Gerente só poderá editar o campo de resposta à reclamação
