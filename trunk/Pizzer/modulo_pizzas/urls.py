ee97d63571b38d0db4299c0f7f52ae12cce16d3e
daniel.interliche <daniel.interliche@8169190b-fedb-79aa-6a5a-a987b9c8a9c4>
2010-05-26 12:27:03 +0000

    Concertei os erros e adicionei a funcionalidade 'edita' no módulo pizza

--- trunk/Pizzer/modulo_pizzas/urls.py
+++ trunk/Pizzer/modulo_pizzas/urls.py
@@ -1,15 +1,30 @@
 from django.conf.urls.defaults import *
-from django.views.generic.create_update import create_object
+from django.views.generic.create_update import create_object, update_object, delete_object
 from django.views.generic.list_detail import object_list
 from modulo_pizzas.models import Pizza
 from views import *
 
-model_info = {
+create_object_info = {
     "model": Pizza,
     "template_name": "criacao_pizza.html"
 }
 
+update_object_info = {
+    "model": Pizza,
+    "template_name": "edicao_pizza.html"
+}
+
+delete_object_info = {
+    "model": Pizza, "template_name": "confirmacao_delecao.html",
+    "post_delete_redirect": "/pizzer/pizzas/",
+    "extra_context": {'model': Pizza}
+}
+
 urlpatterns = patterns('',
-    (r'^pizza/cria/', create_object, model_info),
+    (r'^pizza/cria/$', create_object, create_object_info),
+    (r'^pizza/edita/(?P<object_id>\d+)/$', update_object, update_object_info),
+    (r'^pizza/deleta/(?P<object_id>\d+)/$', delete_object, delete_object_info),
     (r'^pizzas/', lista_pizzas),
+
+
 )
