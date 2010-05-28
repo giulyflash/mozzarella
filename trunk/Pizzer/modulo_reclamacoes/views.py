ea49d144870547dab9a53e7f8e3a1dab05ed65a6
dmail07 <dmail07@8169190b-fedb-79aa-6a5a-a987b9c8a9c4>
2010-05-28 12:31:15 +0000

    um bug aqui outro lá

--- trunk/Pizzer/modulo_reclamacoes/views.py
+++ trunk/Pizzer/modulo_reclamacoes/views.py
@@ -1,7 +1,9 @@
 # -*- coding: utf-8 -*-
 
-from models import Reclamacao
 from django.db.models import Q
+from django.shortcuts import render_to_response
+
+from models import Reclamacao
 
 from utils.views import lista_objetos
 
@@ -10,3 +12,7 @@ def lista_reclamacoes(request):
     texto = request.GET.get('texto')
     consulta = Q(assunto__icontains=assunto) & Q(texto__icontains=texto)  # Busca pelo assunto especificado e/ou um trecho dentro do texto
     return lista_objetos(request, [assunto, texto], Reclamacao, 'listagem_reclamacoes.html', 'reclamacoes', consulta)
+
+def resolve_reclamacao(request, object_id):
+    reclamacao = Reclamacao.objects.get(pk=object_id)
+    return render_to_response('resolucao_reclamacao.html', {'reclamacao': reclamacao})
