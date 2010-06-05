8550f7f824b35714a724f942728f2c2203858620
dmail07 <dmail07@8169190b-fedb-79aa-6a5a-a987b9c8a9c4>
2010-06-05 17:52:46 +0000

    Consertei problema ultra bizarro da renderização do edita cliente no Internet Explorer
    Limpezas no base.html

--- Pizzer/modulo_clientes/views.py
+++ Pizzer/modulo_clientes/views.py
@@ -86,7 +86,7 @@ def edita_cliente(request, object_id):
         cliente = user.cliente_set.all()[0]
         if not user.has_perm('modulo_funcionarios.pode_editar_qualquer_cliente') and cliente.id != int(object_id):
             return HttpResponseRedirect('/pizzer/usuario/login/')
-    return update_object(request, None, object_id, template_name='edicao_cliente.html', form_class=ClienteForm)
+    return update_object(request, object_id=object_id, template_name='edicao_cliente.html', form_class=ClienteForm)
 
 @permission_required('modulo_clientes.pode_deletar_cliente')
 @login_required
