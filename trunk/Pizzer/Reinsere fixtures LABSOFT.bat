03e05f6a93ebf0d9d5f84d97712371b55797f10a
dmail07 <dmail07@8169190b-fedb-79aa-6a5a-a987b9c8a9c4>
2010-06-05 21:43:02 +0000

    dois atalhos para refazer o banco sem deletá-lo antes

---
+++ trunk/Pizzer/Reinsere fixtures LABSOFT.bat
@@ -0,0 +1,8 @@
+cls
+@echo off
+path = %~dp0Python26;%~dp0\Python26\Scripts;%~dp0\Python26\Lib\site-packages\django\bin;%~dp0\Utilities;%~dp0\Utilities\Exemaker;%~dp0\Utilities\Npp;%~dp0\Utilities\Sqlite;%~dp0\Utilities\Mercurial;%~dp0\Utilities\Winmerge;%PATH%
+set PYTHONPATH=%~dp0%\Python26;%~dp0%\Python26\Lib\site-packages
+exemaker -i "%~dp0\Python26\python.exe" "%~dp0\Python26\Lib\site-packages\django\bin\django-admin.py" "%~dp0\Python26\Lib\site-packages\django\bin" 1>nul 2>&1
+echo.
+python manage.py syncdb --noinput
+pause
